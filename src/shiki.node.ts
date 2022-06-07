146fd3968c5d5fe0764e88ef6563864bf898a673
Nicholas Berlette <nick@berlette.com>
2022-06-07 13:11:40 -0700

    ⚡️refactor, drop deprecated WASM method

--- src/shiki.node.ts
+++ src/shiki.node.ts
@@ -1,29 +1,27 @@
-import { setCDN, setWasm } from './shiki'
-export * from './shiki'
 import assets from '../gen/assets'
 import wasm from '../gen/onig'
+import { setCDN, setWasm } from './shiki'
+export * from './shiki'
 
 setCDN('-')
-globalThis.__shiki_fetch__ = async (url) => {
-  url = url.substr(1)
+
+globalThis.__shiki_fetch__ = async (url: string) => {
+  url = url.slice(1)
   const _asset = assets[url]
-  if (!_asset) {
+  if (!_asset)
     throw new Error(`Unknown asset: ${url}`)
-  }
   return {
-    text: () => _asset().then(r => JSON.stringify(r.default || r))
+    text: () => _asset().then((r: any) => JSON.stringify(r.default || r)),
   }
 }
 
-
-setWasm(toArrayBuffer(wasm()))
-
-function toArrayBuffer(base64) {
+function toArrayBuffer(base64: string) {
   const buf = Buffer.from(base64, 'base64')
   const ab = new ArrayBuffer(buf.length)
   const view = new Uint8Array(ab)
-  for (let i = 0; i < buf.length; ++i) {
+  for (let i = 0; i < buf.length; ++i)
     view[i] = buf[i]
-  }
   return ab
 }
+
+setWasm(toArrayBuffer(wasm()))
